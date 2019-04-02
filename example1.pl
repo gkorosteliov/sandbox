727237f606d71d3aa709e59305191b54463416f1
gkorosteliov <48071315+gkorosteliov@users.noreply.github.com>
2019-04-02 10:31:41 -0400

    sub for processing file

--- example1.pl
+++ example1.pl
@@ -1,20 +1,5 @@
 #!/usr/bin/perl -w
 
-$| = 1;
-undef $/;
-
-if ($#ARGV < 0)
-{
-	usage();
-}
-my $in_file = $ARGV[0];
-my $out_file = $in_file."out";
-
-open (IN, "<$in_file");
-my $content = <IN>;
-close (IN);
-#!/usr/bin/perl -w
-
 use strict;
 use warnings;
 
@@ -60,22 +45,6 @@ sub process_file
 	
 	return $cont;
 }
-
-sub usage 
-{
-	print "*********\nUsage:\n";
-	print "example1.pl <input_file>\n";
-	print "Example:\n";
-	print "example.pl test.txt\n*********\n";	
-	exit 0;
-}
-$content =~ s/bookpart/BOOKPART/sg;
-print $content."\n";
-
-open (OUT, ">$out_file");
-print OUT $content;
-close (OUT);
-exit 0;
 
 sub usage 
 {
