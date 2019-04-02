a43d32bd749168bf9d36e0d1de11660f273cb2af
gkorosteliov <48071315+gkorosteliov@users.noreply.github.com>
2019-04-02 10:30:19 -0400

    sub for processing file

--- example1.pl
+++ example1.pl
@@ -13,7 +13,62 @@ my $out_file = $in_file."out";
 open (IN, "<$in_file");
 my $content = <IN>;
 close (IN);
+#!/usr/bin/perl -w
 
+use strict;
+use warnings;
+
+$| = 1;
+undef $/;
+
+if ($#ARGV < 0)
+{
+	usage();
+}
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+$year =~ s/^1/20/;
+print "Time: $hour:$min:$sec $mday/$mon/$year\n";
+
+my $in_file = $ARGV[0];
+my $out_file = $in_file."out";
+
+if (! -e $in_file)
+{
+	print STDERR "Error: $in_file does not exist.\n";
+	exit 1;
+}
+
+open (IN, "<$in_file");
+my $content = <IN>;
+close (IN);
+
+$content=process_file($in_file, $content);
+
+open (OUT, ">$out_file");
+print OUT $content;
+close (OUT);
+exit 0;
+
+sub process_file
+{
+	my($file_name,$cont)=@_;
+	
+	my $counter = ($cont =~ s/bookpart/BOOKPART/sg);
+	print $cont."\n";
+	print "There were $counter replacements in $file_name\n";
+	
+	return $cont;
+}
+
+sub usage 
+{
+	print "*********\nUsage:\n";
+	print "example1.pl <input_file>\n";
+	print "Example:\n";
+	print "example.pl test.txt\n*********\n";	
+	exit 0;
+}
 $content =~ s/bookpart/BOOKPART/sg;
 print $content."\n";
 
