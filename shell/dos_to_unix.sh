47a2661fb30744325476e6c4baca5b8cfe69d986
gkorosteliov <48071315+gkorosteliov@users.noreply.github.com>
2019-04-03 12:45:17 -0400

    use for and foreach

--- shell/dos_to_unix.sh
+++ shell/dos_to_unix.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 foreach x ( *.html)
-echo $x
-tr -d '\r' < $x > foo/$x
+  echo $x
+  tr -d '\r' < $x > foo/$x
 end
+
+for filename in *.html
+do
+  echo $filename
+  cp $filename $filename.bak
+done
